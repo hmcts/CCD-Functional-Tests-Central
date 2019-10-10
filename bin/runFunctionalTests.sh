bdeeb6c2909155269d3550983c6d0c9d5d90201f
Hemanth Potipati <hrpotipati@gmail.com>
2019-10-10 15:07:41 +0100

    Create runFunctionalTests.sh

---
+++ bin/runFunctionalTests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ [[ -z $CCD_CASEWORKER_AUTOTEST_FE_EMAIL ]] && echo "CCD_CASEWORKER_AUTOTEST_FE_EMAIL is not set";
+ [[ -z $CCD_CASEWORKER_AUTOTEST_FE_PASSWORD ]] && echo "CCD_CASEWORKER_AUTOTEST_FE_PASSWORD is not set";
+ [[ -z $TEST_URL ]] && echo "TEST_URL is not set" || echo "TEST_URL=$TEST_URL";
+
+if [[ ${TEST_URL} != *"preview-staging"* ]];then
+        yarn run test:functionalTests
+else
+        echo "THIS IS A PREVIEW ENV, NOT RUNNING FUNCTIONAL TESTS DUE TO GREY SCREEN ISSUE ON PREVIEW"
+fi
